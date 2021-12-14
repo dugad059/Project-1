b3df2c250511cded1b481342029bb974fac4d51b
David Dugas <daviddugas@Davids-MacBook-Pro.local>
2021-12-14 14:21:32 -0600

    Basic JS COMPLETE

--- script.js
+++ script.js
@@ -6,46 +6,55 @@ const settings = {
     "method": "GET",
     "headers": {
         "x-rapidapi-host": "exercisedb.p.rapidapi.com",
-        "x-rapidapi-key": "d2d1a1c573msh70dec9a00978dd2p1d89c4jsn6b5434d2a868"
+        "x-rapidapi-key": "561fa1cdadmsh061526f3ccafe2cp1f6fafjsn0cac3358fd80"
     }
 };
 
 
-// Element Refs
 
 
-// Event Listeners
-$(document).ready(function() {
-$('.select').on('change', function () {
-    const value = $(this).val();
-    console.log(value)
 
+
+$(document).ready(function () {
+    $('.select').on('change', function () {
+        const value = $('.select').val();
+        console.log(value)
+
+        $.ajax(settings).then(function (data) {
+            console.log(data)
+
+            const filtered = data.filter(function(ddlSelector){
+                    return ddlSelector.target === value
+            } )
+                console.log(filtered)
+
+            let html = '';
+            filtered.forEach(element => {
+                let card = `<div class="card">
+                        <div class="card-body">
+                        <h5 class="card-title">Exercise</h5>
+                        <p id="name"> Exercise Name: ${element.name}</p>
+                        <p id="bodyPart">Body Part: ${element.target}</p>
+                        <p id="equipment">Equipment: ${element.equipment}</p>
+                        </div>
+                      </div>`
+
+                html = html.concat(card);
+
+                $('main').empty().append(html);
+
+
+
+            });
+
+
+        })
+    })
 })
-})
 
-// Functions 
 
-$.ajax(settings).then(function (response) {
-    console.log(response);
-    let html = '';
-    response.forEach(element => {
-        let card = `<div class="card">
-                    <div class="card-body">
-                    <h5 class="card-title">Exercise</h5>
-                    <p id="name"> Exercise Name: ${element.name}</p>
-                    <p id="bodyPart">Body Part: ${element.target}</p>
-                    <p id="equipment">Equipment: ${element.equipment}</p>
-                    </div>
-                  </div>`  
 
-    html = html.concat(card);
 
-    $(main).empty().append(html);
-
-   
-        
-    });
-})
 
 
 
